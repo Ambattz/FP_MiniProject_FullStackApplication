465ecdbcf16fff118df2ba3c9a720d74f083a386
[Ambattz} <[alvingeorge30@gmail.com]>
2022-05-12 02:12:25 +0530

    PATCH, DELETE completed

--- ReactJS/src/App.js
+++ ReactJS/src/App.js
@@ -4,12 +4,10 @@ import "./App.css"
 
 
 class Home extends Component {
-
   state = {
     show: false,
     data: [],
     rating: 1,
-    isFetching: false,
   }
   componentDidMount = () => {
     // Write your code here
@@ -24,15 +22,15 @@ class Home extends Component {
 
   handleGetData = () => {
     // Write your code here
-    this.setState({ ...this.state, isFetching: true });
+    this.setState({ ...this.state, show: true });
     fetch("/courses/get")
       .then(response => response.json())
       .then(result => {
-        this.setState({ data: result, isFetching: false })
+        this.setState({ data: result, show: false })
       })
       .catch(e => {
         console.log(e);
-        this.setState({ ...this.state, isFetching: false });
+        this.setState({ ...this.state, show: false });
       });
   };
 
@@ -55,10 +53,33 @@ class Home extends Component {
 
   handleRating = (e) => {
     // Write your code here
+    this.setState({ rating: e.target.value });
   }
 
   handleAddRating = async (id) => {
     // Write your code here
+    this.state.data[id].isRated = true;
+    this.state.data[id].noOfRatings = this.state.data[id].noOfRatings + 1;
+    this.state.data[id].rating = (((this.state.data[id].rating * this.state.data[id].noOfRatings) + this.state.rating) / (this.state.data[id].noOfRatings + 1)).toFixed(1)
+
+    this.setState({
+      ...this.state,
+      data: this.state.data
+    })
+    var url = '/courses/rating/' + this.state.data[id]["_id"];
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        "rating": this.state.rating
+      })
+    };
+    fetch(url, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        alert(data["message"]);
+        this.handleGetData();
+      });
   }
 
   handleDrop = async (id) => {
@@ -87,7 +108,7 @@ class Home extends Component {
           <h2>ABC Learning</h2>
         </header>
         {
-          this.state.isFetching ? "Fetching DATA" :
+          this.state.show ? "Fetching DATA" :
             this.state.data.map(
               (course, index) => (
                 <div className="cardContainer" key={index}>
@@ -101,16 +122,22 @@ class Home extends Component {
                           course.isApplied ?
                             (
                               <li>
-                                <li> Rate :
-                                  <select className="rating" name="rating">
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
-                                  </select>
-                                  <button className="rate">Add</button>
-                                </li>
+                                {
+                                  !course.isRated ?
+                                    (
+                                      <li> Rate :
+                                        <select className="rating" name="rating" onChange={(e) => { this.handleRating(e) }}>
+                                          <option>1</option>
+                                          <option>2</option>
+                                          <option>3</option>
+                                          <option>4</option>
+                                          <option>5</option>
+                                        </select>
+                                        <button className="rate" onClick={() => { this.handleAddRating(index) }}>Add</button>
+                                      </li>
+                                    ) :
+                                    []
+                                }
                                 <button className="drop" onClick={() => { this.handleDrop(index) }}>Drop Course</button>
                               </li>
                             ) :
